d2842f41b5d6d2d7bca5d6b585b59843d16ed7b6
wlsrud <jinkyeont123@naver.com>
2018-12-09 09:37:01 -0800

    재지향 >< 탐색 구현

--- team.c
+++ team.c
@@ -18,3 +18,50 @@ int SetSignal(struct sigaction *def, sigset_t *mask, void(*handler)(int)) { //�
   		return -1;
  	return 0;
 }
+
+//재지향 >< 탐색 구현
+
+int FindRedirectIn(char *cmd) { // < 찾고 분석
+ 	int error;
+ 	int infd;
+ 	char *infile;
+
+ 	if ((infile = strchr(cmd, '<')) == NULL)
+  		return 0;
+ 	*infile = 0;
+ 	infile = strtok(infile + 1, " \t");
+ 	if (infile == NULL)
+  		return 0;
+ 	if ((infd = open(infile, O_RDONLY)) == -1)
+  		return -1;
+ 	if (dup2(infd, STDIN_FILENO) == -1) {
+  		error = errno;
+  		close(infd);
+  		errno = error;
+  		return -1;
+ 	}
+ 	return close(infd);
+}
+
+int FindRedircetOut(char *cmd) { // > 찾고 분석
+ 	int error;
+ 	int outfd;
+ 	char *outfile;
+
+ 	if ((outfile = strchr(cmd, '>')) == NULL)
+  		return 0;
+ 	*outfile = 0;
+ 	outfile = strtok(outfile + 1, " \t");
+ 	if (outfile == NULL)
+  		return 0;
+ 	if ((outfd = open(outfile, O_WRONLY)) == -1)
+  		return -1;
+ 	if (dup2(outfd, STDOUT_FILENO) == -1) {
+  		error = errno;
+  		close(outfd);
+  		errno = error;
+  		return -1;
+ 	}
+ 	return close(outfd);
+}
+
