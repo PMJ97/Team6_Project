b7c4b82228c515da4a67db327ec3ec641d70eba1
PMJ97 <alwls2552@naver.com>
2018-12-09 09:44:12 -0800

    파이프 기능 구현

--- team.c
+++ team.c
@@ -65,3 +65,39 @@ int FindRedircetOut(char *cmd) { // > 찾고 분석
  	return close(outfd);
 }
 
+
+void Command_exe(char *cmds) {
+ 	int child;
+ 	int count;
+ 	int fds[2];
+ 	int i;
+ 	char **pipelist;
+
+ 	count = ArgvPointer(cmds, "|", &pipelist);
+ 	if (count <= 0) {
+  		fprintf(stderr, "Failed\n");
+  		exit(1);
+ 	}
+ 	for (i = 0; i < count - 1; i++) {
+  		if (pipe(fds) == -1)
+   			perror_exit("Failed to create pipes");
+  		else if ((child = fork()) == -1)
+   			perror_exit("Failed to create process to run command");
+  		else if (child) {
+   			if (dup2(fds[1], STDOUT_FILENO) == -1)
+    				perror_exit("Failed to connect pipeline");
+   			if (close(fds[0]) || close(fds[1]))
+    				perror_exit("Failed to close needed files");
+
+   			FileRedirect(pipelist[i], i == 0, 0);
+   			exit(1);
+  		}
+  		if (dup2(fds[0], STDIN_FILENO) == -1)
+   			perror_exit("Failed to connect last component");
+ 		if (close(fds[0]) || close(fds[1]))
+   			perror_exit("Failed to do final close");
+ 	}
+ 	FileRedirect(pipelist[i], i == 0, 1);
+ 	exit(1);
+
+}
