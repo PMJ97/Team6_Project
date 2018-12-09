9804c93f8b95a6967a75ec6375d24a47216d5dc0
PMJ97 <alwls2552@naver.com>
2018-12-09 10:10:41 -0800

    main함수 수정 (소스 통합)

--- team.c
+++ team.c
@@ -1,11 +1,31 @@
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <limits.h>
 #include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <sys/stat.h>
 #include <setjmp.h>
+
+#define MAX_CANON 256
+#define QUIT_STRING "q"
+#define BLANK_STRING " "
+#define PROMPT_STRING "[Team_6]SHELL>> "
+#define BACK_SYMBOL '&'
+
+void FileRedirect(char *s, int in, int out); //4번 파일 재지향
+void Command_exe(char *); //시스템 명령 실행
+int ArgvPointer(const char *s, const char *delimiters, char ***argvp); //문자열 포인터배열로 정의
+int SetSignal(struct sigaction *def, sigset_t *mask, void(*handler)(int)); //시그널 설정
+int FindRedirectIn(char *cmd); // < 찾고 분석
+int FindRedircetOut(char *cmd); // > 찾고 분석
+static void perror_exit(char *s);
+
+int desc[2];
 
 static sigjmp_buf ToPrompt;
 static volatile sig_atomic_t IndexJump = 0;
@@ -17,21 +37,114 @@ static void jumphd(int signalnum) {
  	siglongjmp(ToPrompt, 1);
 }
 
-int main(){
-	if (strcmp(str[0], "cd") == 0) {
+int main(void) {
+ 	pid_t childpid;
+ 	char inbuf[MAX_CANON];
+ 	int len;
+ 	sigset_t blockmask;
+ 	struct sigaction defhandler;
+ 	char *backp;
+ 	int inbackground;
+ 	char **str;
+ 	int tcnt = 0;
+ 	char pipebuf[101];
+ 	int j, k;
+ 	int str_len;
+ 	int hcnt = 0, st = 0;
+ 	int recordok = 0;
+
+ 	pipe(desc);
+
+ 	if (SetSignal(&defhandler, &blockmask, jumphd) == -1) {
+  		perror("Failed to set up shell signal handling");
+  		return 1;
+ 	}
+
+ 	if (sigprocmask(SIG_BLOCK, &blockmask, NULL) == -1) {
+  		perror("Failed to block signals");
+  		return 1;
+ 	}
+ 	alarm(600);
+
+ 	while (1) {
+
+  		if ((sigsetjmp(ToPrompt, 1)) && (fputs("\n", stdout) == EOF))
+   			continue;
+  		IndexJump = 1;
+
+  		if (fputs(PROMPT_STRING, stdout) == EOF)
+   			continue;
+  		if (fgets(inbuf, MAX_CANON, stdin) == NULL)
+  			continue;
+
+  		len = strlen(inbuf);
+
+  		if (inbuf[len - 1] == '\n')
+   			inbuf[len - 1] = 0;
+  		if (strcmp(inbuf, QUIT_STRING) == 0)
+   			break;
+  		if ((backp = strchr(inbuf, BACK_SYMBOL)) == NULL)
+   			inbackground = 0;
+  		else {
+   			inbackground = 1;
+   			*backp = 0;
+  		}
+  		if (sigprocmask(SIG_BLOCK, &blockmask, NULL) == -1)
+   			perror("Failed to block signals");
+
+ 
+
+  		ArgvPointer(inbuf, " \t", &str);
+
+  		if (strcmp(str[0], "cd") == 0) {
    			chdir(str[1]);
    			system("pwd");
 
   			continue;
   		}
-	if (strcmp(str[0], "exit") == 0) {
+
+		if (strcmp(str[0], "exit") == 0) {
 
    			return 0;
   		}
+
+  		for (j = 0; j < 100; j++)
+   			pipebuf[j] = '\0';
+
+  		write(desc[1], " ", sizeof(char));
+  		str_len = read(desc[0], pipebuf, 100);
+  		pipebuf[str_len] = 0;
+
+  		printf("[%s | %d] \n", pipebuf, str_len);
+
+  		if ((childpid = fork()) == -1) {
+   			perror("Failed to fork child to execute command");
+  		}
+  		else if (childpid == 0) {
+   			if (inbackground && (setpgid(0, 0) == -1))
+    				return 1;
+   			if ((sigaction(SIGINT, &defhandler, NULL) == -1) || (sigaction(SIGQUIT, &defhandler, NULL) == -1) || (sigprocmask(SIG_UNBLOCK, &blockmask, NULL) == -1)) {
+    				perror("Failed to set signal handling for command ");
+    				return 1;
+   			}
+   			Command_exe(inbuf);
+   			return 1;
+  		}
+  		if (sigprocmask(SIG_UNBLOCK, &blockmask, NULL) == -1)
+   			perror("Failed to unblock signals");
+  		if (!inbackground)
+   			waitpid(childpid, NULL, 0);
+  		while (waitpid(-1, NULL, WNOHANG) > 0);
+ 	}
+ 	return 0;
 }
 
-//문자열 포인터배열로 정의
-int ArgvPointer(const char *s, const char *delimiters, char ***argvp) {
+static void perror_exit(char *s) {
+ 	perror(s);
+ 	exit(1);
+}
+
+int ArgvPointer(const char *s, const char *delimiters, char ***argvp) { //문자열 포인터배열로 정의
  	int error;
  	int i;
  	int numtokens;
@@ -69,20 +182,6 @@ int ArgvPointer(const char *s, const char *delimiters, char ***argvp) {
  	*((*argvp) + numtokens) = NULL;
  	return numtokens;
 }
-
-int SetSignal(struct sigaction *def, sigset_t *mask, void(*handler)(int)) { //시그널 설정
- 	struct sigaction catch;
-
- 	catch.sa_handler = handler;
- 	def->sa_handler = SIG_DFL;
- 	catch.sa_flags = 0;
- 	def->sa_flags = 0;
- 	if ((sigemptyset(&(def->sa_mask)) == -1) || (sigemptyset(&(catch.sa_mask)) == -1) || (sigaddset(&(catch.sa_mask), SIGINT) == -1) || (sigaddset(&(catch.sa_mask), SIGQUIT) == -1) || (sigaction(SIGINT, &catch, NULL) == -1) || (sigaction(SIGQUIT, &catch, NULL) == -1) || (sigemptyset(mask) == -1) || (sigaddset(mask, SIGINT) == -1) || (sigaddset(mask, SIGQUIT) == -1))
-  		return -1;
- 	return 0;
-}
-
-//재지향 >< 탐색 구현
 
 int FindRedirectIn(char *cmd) { // < 찾고 분석
  	int error;
@@ -128,8 +227,7 @@ int FindRedircetOut(char *cmd) { // > 찾고 분석
  	return close(outfd);
 }
 
-
-void Command_exe(char *cmds) {
+void Command_exe(char *cmds) { //시스템 명령 실행
  	int child;
  	int count;
  	int fds[2];
@@ -165,7 +263,7 @@ void Command_exe(char *cmds) {
 
 }
 
-void FileRedirect(char *s, int in, int out) { //4번 및 파일 재지향
+void FileRedirect(char *s, int in, int out) { //4번 파일 재지향
  	char **chargv;
  	char *pin;
  	char *pout;
@@ -199,3 +297,17 @@ void FileRedirect(char *s, int in, int out) { //4번 및 파일 재지향
  	}
  	exit(1);
 }
+
+int SetSignal(struct sigaction *def, sigset_t *mask, void(*handler)(int)) { //시그널 설정
+ 	struct sigaction catch;
+
+ 	catch.sa_handler = handler;
+ 	def->sa_handler = SIG_DFL;
+ 	catch.sa_flags = 0;
+ 	def->sa_flags = 0;
+ 	if ((sigemptyset(&(def->sa_mask)) == -1) || (sigemptyset(&(catch.sa_mask)) == -1) || (sigaddset(&(catch.sa_mask), SIGINT) == -1) || (sigaddset(&(catch.sa_mask), SIGQUIT) == -1) || (sigaction(SIGINT, &catch, NULL) == -1) || (sigaction(SIGQUIT, &catch, NULL) == -1) || (sigemptyset(mask) == -1) || (sigaddset(mask, SIGINT) == -1) || (sigaddset(mask, SIGQUIT) == -1))
+  		return -1;
+ 	return 0;
+}
+
+
